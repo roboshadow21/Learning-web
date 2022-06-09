1b7843a3c0edec7f56ea3cba584134a5f3ed2935
Alexei Yurikov <roboshadow@mail.ru>
2022-06-09 22:11:07 +0300

    more fixes in js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,7 @@
-// Создаем переменную, в нее добавляем поиск элемента по ID
+//Создаем переменную, в которую добавляем поиск элемента по его id
 let toggleThemeBtn = document.getElementById('toggle-theme-btn')
-// Пишем обработку события
-//toggleThemeBtn.addEventListener('click', function(){
-//  document.body.classList.add('dark')
-//})
-toggleThemeBtn.addEventListener('click', function() {
-document.body.classList.toggle('dark');
+//Пишем обработку события
+toggleThemeBtn.addEventListener('click', function(){
+  document.body.classList.toggle('dark');
+
 })
