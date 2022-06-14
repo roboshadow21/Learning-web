c3894ad61ca77e37685e431b498c60931ccc72c5
Alexei Yurikov <roboshadow@mail.ru>
2022-06-14 21:51:59 +0300

    add contacts and datainfo.php files

---
+++ contacts.php
@@ -0,0 +1,9 @@
+<?php
+  return [
+  'contacts' => [
+    'address' => '7404 Howell Cliffs Apt. 438 Lake Royceville, MI 19821-9898',
+    'e-male' => 'bartell.abner@cormier.org',
+    'phone' => 'phone: 266(36)060-47-69'
+  ]
+  ];
+?>
